f475197d85b059eeb9a5bdf3cab3c9b97c1d2a8d
ManepuAnusri <71380028+ManepuAnusri@users.noreply.github.com>
2021-05-31 17:43:50 +0530

    my commit

---
+++ inheritance/src/inheritance/Calculator.java
@@ -0,0 +1,19 @@
+package inheritance;
+
+public class Calculator {
+	public int add(int a,int b)
+	   {
+		   return a+b;
+		   
+	   }
+	}
+	class calcadv extends Calculator{
+		public int sub(int a,int b)
+		{
+			return a-b;
+		}
+	}
+
+
+
+
---
+++ inheritance/src/inheritance/Myself.java
@@ -0,0 +1,14 @@
+package inheritance;
+
+public class Myself {
+ public static void main(String args[]) {
+ 
+	calcadv obj= new calcadv();
+	int  result1=obj.add(2,9);
+	int result2=obj.sub(7,5);
+	System.out.println(result1);
+	System.out.println(result2);
+ }	
+
+}
+  
